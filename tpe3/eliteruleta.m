304fd021cbbf50acf373feb49b7f62e04c81b492
lucasfarre <lucasfarre@gmail.com>
2015-05-29 18:31:46 -0300

    Se cambio el default de los mixed

--- tpe3/eliteruleta.m
+++ tpe3/eliteruleta.m
@@ -1,4 +1,4 @@
-function ret = eliteruleta(k,poblation,N1=10)
+function ret = eliteruleta(k,poblation,N1=0)
 	poblation_elite = elite(N1, poblation);
 	poblation_ruleta = ruleta(k - N1, poblation);
 	ret = [poblation_elite poblation_ruleta];
