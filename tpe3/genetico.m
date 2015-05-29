2366a515db1c95ec021da4721ff8e007b348b0cc
lbadi <leonelbadi@hotmail.com>
2015-05-29 19:18:07 -0300

    Se agregaron las razones de corte, solo falta hacer por estructura

--- tpe3/genetico.m
+++ tpe3/genetico.m
@@ -3,7 +3,7 @@
 % hidenN = Cantidad de neuronas en la capa oculta
 % k = cantidad de seleccionados.
 % iterations = cantidad de iteraciones.
-function ret = genetico(n,hidenN,k,iterations)
+function ret = genetico(n,hidenN,k,iterations,fitnessWish = 100, timeToMakeProgress = 1000)
 	% Elegir una población random
 	for i=1:n
 		poblation(i).w1 = rand(2,hidenN);
@@ -12,16 +12,41 @@ function ret = genetico(n,hidenN,k,iterations)
 	% Calcularle la aptitud a esa población y hacerle backpropagation un poco
 	% Ver de que manera es mas conveniente guardar el fitness
 	poblation = fitness(poblation);
-	% Calcular el fitness relativo
-	for i=1:iterations
-	 	poblation = replaceMixed(poblation,@torneos,@onePointCross,@mutateLineal,0.1,@ruleta,4,@torneos,5);
+	betterElement.fitness = 0;
+	betterFitnessOfAllGenerations = 0;
+	timeFromLastImprove = 0;
+	i = 0;
+	while(1)
+		% Checkeo las razones para terminar
+		if(i > iterations)
+			reassonToEnd = "Maxima cantidad de generaciones alcanzadas";
+			break;
+		end
+		if(betterElement.fitness >= fitnessWish)
+			reassonToEnd = "Se alcanzo un elemento suboptimo con el fitness deseado";
+			break;
+		end
+		if(timeFromLastImprove>= timeToMakeProgress)
+			reassonToEnd = "No hubo progreso en mas generaciones que las deseadas";
+			break;
+		end
+
+	 	poblation = replaceThree(poblation,@eliteruleta,@onePointCross,@mutateLineal,0.1,@ruleta,16);
 	 	totalFit = 0;
 	 	betterElement.fitness = 0;
+
+	 	% Veo quien tiene mejor fitness
 	 	for j = 1 : length(poblation(:))
 	 		totalFit += poblation(j).fitness;
 	 		if(poblation(j).fitness > betterElement.fitness)
 	 			betterElement = poblation(j);
 	 		end
+	 	end
+	 	% Si hago un progreso en el fitness vuelvo a resetear el tiempo.
+	 	timeFromLastImprove++;
+	 	if(betterFitnessOfAllGenerations < betterElement.fitness)
+	 		timeFromLastImprove = 0;
+	 		betterFitnessOfAllGenerations = betterElement.fitness;
 	 	end
 	 	printf("Generacion numero : %d \n" , i);
 	 	totalFit
@@ -31,10 +56,11 @@ function ret = genetico(n,hidenN,k,iterations)
 	 	end
 	 	% Calcualar el maximo de los fitness de la poblacion
 	 	% max(cat(1,poblation.fitness))
-
+	 	i++;
 	 	fflush(stdout);
 	end
-	ret = betterElement.fitness;
+	printf("La razon para terminar : %s \n", reassonToEnd);
+	ret = betterElement;
 
 	
 
