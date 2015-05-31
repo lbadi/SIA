8cf9126d3ec4e57e7cdf9d29ff13af3011f34717
Jorge Gómez <gomezjorge.exequiel@gmail.com>
2015-05-31 18:16:48 -0300

    Algoritmo y configuraciones listas para realizar pruebas.

--- tpe3/restructureChild.m
+++ tpe3/restructureChild.m
@@ -3,8 +3,7 @@ function ret = restructureChild(e1,e2,child1,child2)
 	child(1).w2 = toMatrix(child1(rows(e1.w1) * columns(e1.w1) + 1: length(child1)),1,rows(e1.w2) * columns(e1.w2));
 	child(2).w1 = toMatrix(child2(1:rows(e2.w1) * columns(e2.w1)),columns(e2.w1),2);
 	child(2).w2 = toMatrix(child2(rows(e2.w1) * columns(e2.w1) + 1: length(child2)),1,rows(e1.w2) * columns(e1.w2));
-	eta = etaCross(e1.eta,e2.eta)
-	fflush(stdout);
+	eta = etaCross(e1.eta,e2.eta);
 	child(1).eta = eta;
 	child(2).eta = eta;
 	ret = child;
